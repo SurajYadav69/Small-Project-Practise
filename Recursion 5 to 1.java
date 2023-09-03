32b453aba93283b18f58f06405b9887f264c0d97
Suraj Yadav <136815807+SurajYadav69@users.noreply.github.com>
2023-09-03 22:56:23 +0530

    Create Recursion 5 to 1.java
    it's my first recursion program.

---
+++ Recursion 5 to 1.java
@@ -0,0 +1,17 @@
+package DSA.Recursion;
+
+public class Recursion5To1 {
+
+    public static void printNum(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNum(n);
+    }
+}
