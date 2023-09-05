eab206193041f8987b54420fb3b2e22775523ad1
Suraj Yadav <136815807+SurajYadav69@users.noreply.github.com>
2023-09-05 22:38:11 +0530

    Sum Of Factorial Of N Natural Number From User Input.java

--- Sum Of Factorial Of N Natural Number From User.java
+++ Sum Of Factorial Of N Natural Number From User.java
@@ -1,1 +1,22 @@
-S
+package DSA.Recursion;
+
+import java.util.Scanner;
+
+public class RecursionOf_N_Factorial {
+    public static int Sumfac(int n) {
+        if(n==1 || n==0){
+            return 1;
+        }
+        int fac_1 = Sumfac(n-1);
+        int fac_2 = n*fac_1;
+        return fac_2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:- ");
+        int n = sc.nextInt();
+        int ans = Sumfac(n);
+        System.out.println("Sum Of Factorial NAtural No Is :- "+ans);
+
+    }
+}
