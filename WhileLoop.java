9e2ee88f10adff50903f169ee1e8fd84198db63a
Suraj Yadav <136815807+SurajYadav69@users.noreply.github.com>
2023-12-26 15:13:48 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+package REVISION;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 100;
+        while(  i <=200) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
